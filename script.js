5ad6350ade665ab304ab404a4e87369066e54770
mrvijaychandrenstrkom-sys <mr.vijaychandren.s.tr.kom@gmail.com>
2026-07-22 10:31:21 +0700

    Add event listener to show main content

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const button = document.getElementById("openInvitation");
+const cover = document.getElementById("cover");
+const mainContent = document.getElementById("mainContent")
+button.addEventListener("click", function(){
+  cover.style.display = "none";
+  mainContent.style.display = "block";
+});
