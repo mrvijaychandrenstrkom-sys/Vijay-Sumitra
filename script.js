0104c40aee39d354234c8d418d9986acb75e7e54
mrvijaychandrenstrkom-sys <mr.vijaychandren.s.tr.kom@gmail.com>
2026-07-29 11:25:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,3 @@ button.addEventListener("click", function() {
   mainContent.style.display = "block";
 });
 
-const targetDate = new Date("September 18 ,2026 00:00:00").getTime();
-setInterval (function(){
-  const now = new Date().getTime();
-  const distance = tagetData-now;
-  const days = Math.floor(distance / (1000*60*60*24));
-  const hours = Math.floor(distance % (1000*60*60*24)) / (1000*60*60));
-  const minutes = Math.floow(distance % (1000*60*60)) / (1000*60));
-  conts seconds = Math.floow(distance % (1000*60)) / 1000);
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = Hours;
-  document.getElementById("minutes").innerHTML = minutes;
-  documnet.getElementById("seconds").innerHTML = Seconds;
-},1000);
